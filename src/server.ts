81bb115d4af2946dd7cb457ea148914d0aed198e
rajesh-kayal-dev <rk6289943975@gmail.com>
2026-04-01 11:28:25 +0530

    MongoDB connected and User model created

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import app from './app';
+import { connectDB } from './config/db';
 
 const PORT= process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
+connectDB(). then(() =>{app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
+});
