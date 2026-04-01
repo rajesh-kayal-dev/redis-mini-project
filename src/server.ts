22c39a8f77b945a165b709ee788bc48721ac6946
rajesh-kayal-dev <rk6289943975@gmail.com>
2026-04-01 11:51:35 +0530

    Redis connected to backend

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import app from './app';
 import { connectDB } from './config/db';
+import { connectRedis } from './config/redis';
 
 const PORT= process.env.PORT || 5000;
 
-connectDB(). then(() =>{app.listen(PORT, ()=>{
+connectDB().then(async () =>{
+    await connectRedis();
+    
+    app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
-})
+    })
 });
